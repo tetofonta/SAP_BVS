1d7efa0711d14b966cbc5a187ae8e848b98b4407
Gaffurini Simone <gaffu29@gmail.com>
2018-06-12 08:45:53 +0000

    Fixed buttons css

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -10,7 +10,7 @@ let getSavedMatches = "getSavedMatches";
 let getPlayers = "getPlayers";
 
 
-let apiHost = "http://cors.io/?http://bettervolleyscouting.altervista.org";
+let apiHost = "https://cors.io/?http://bettervolleyscouting.altervista.org";
 
 function getFromApi(res, prams) {
     if (!res.startsWith("/")) res = "/" + res;
@@ -29,9 +29,10 @@ function getFromApi(res, prams) {
 let oModel = {};
 
 sap.ui.define([
+	"jquery.sap.global",
     "sap/ui/core/mvc/Controller",
     'sap/ui/model/json/JSONModel'
-], function (Controller, JSONModel) {
+], function (JQuery, Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("BVS.controller.Home", {
@@ -49,7 +50,7 @@ sap.ui.define([
             if (oEvent.getSource()._lastValue !== "+++ Aggiungi squadra +++") {
                 oModel.setProperty("/saved", getFromApi(getSavedMatches, {SQUADRA: oEvent.getSource()._lastValue}));
                 oModel.setProperty("/players", getFromApi(getPlayers, {SQUADRA: oEvent.getSource()._lastValue}));
-                document.getElementsByClassName("footer")[0].style.opacity = "1.0 !important";
+                $('.showButtons').css('opacity', '1.0', 'important');
             } else {
                 //TODO: aggiungi squadra
             }
